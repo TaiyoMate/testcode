7b3bd54c6a5dae8b365f6dc402ebb2314fc13b13
TaiyoMate <cube4971@gmail.com>
2022-06-27 04:33:50 +0900

    change.

--- lib/main.dart
+++ lib/main.dart
@@ -29,13 +29,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  static const moviesTitles = ['Inception', 'Heat', 'Spider Man'];
 
   @override
   Widget build(BuildContext context) {
@@ -43,24 +37,8 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Column(
+        children: moviesTitles.map((title) => Text(title)).toList(),
       ),
     );
   }
